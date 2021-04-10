6a2cceebcbc530e76638631ca1a7fcd3121966e1
Novanda Ahsan <novandaahsan1@gmail.com>
2021-04-10 08:08:30 +0700

    merge syles config, and minify task

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ const __prod__ = process.env.NODE_ENV === "production";
 const { src, dest, watch, series } = require("gulp");
 
 const noop = require("gulp-noop");
+const merge = require("merge-stream");
 const babel = require("gulp-babel");
 const concat = require("gulp-concat");
 const createGulpEsbuild = require("gulp-esbuild");
@@ -22,39 +23,36 @@ const gulpEsbuild = createGulpEsbuild({
     },
 });
 
-function editorStyle(cb) {
-    return src("./src/**/editor.s[ca]ss")
-        .pipe(sass({ outputStyle: "compressed" }).on("error", sass.logError))
-        .pipe(postcss())
-        .pipe(
-            autoprefixer({
-                browserlist: ["last 2 versions"],
-                cascade: false,
-            })
-        )
-        .pipe(__prod__ ? cleanCSS({ compatibility: "ie8" }) : noop())
-        .pipe(concat("blocks.editor.css"))
-        .pipe(dest("./dist"));
-    cb();
-}
+const styles = [
+    {
+        src: "./src/**/editor.s[ca]ss",
+        distName: "blocks.editor.css",
+    },
+    {
+        src: "./src/**/style.s[ca]ss",
+        distName: "blocks.style.css",
+    },
+];
 
-function feStyle(cb) {
-    return src("./src/**/style.s[ca]ss")
-        .pipe(sass({ outputStyle: "compressed" }).on("error", sass.logError))
-        .pipe(postcss())
-        .pipe(
-            autoprefixer({
-                browserlist: ["last 2 versions"],
-                cascade: false,
-            })
-        )
-        .pipe(__prod__ ? cleanCSS({ compatibility: "ie8" }) : noop())
-        .pipe(concat("blocks.style.css"))
-        .pipe(dest("./dist"));
-    cb();
-}
+const _styles = (cb) => {
+    const task = styles.map((element) =>
+        src(element.src)
+            .pipe(postcss())
+            .pipe(
+                autoprefixer({
+                    browserlist: ["last 2 versions"],
+                    cascade: false,
+                })
+            )
+            .pipe(__prod__ ? cleanCSS({ compatibility: "ie8" }) : noop())
+            .pipe(concat(element.distName))
+            .pipe(dest("./dist"))
+    );
 
-function build(cb) {
+    return merge(task);
+};
+
+function _build(cb) {
     return src("./src/index.js")
         .pipe(
             babel({
@@ -67,12 +65,9 @@ function build(cb) {
 }
 
 function watchTask() {
-    watch("./src/**/*.js", build);
-    watch("./src/**/*.editor[ca]ss", editorStyle);
-    watch("./src/**/*.style[ca]ss", feStyle);
+    watch("./src/**/*.js", _build);
+    watch("./src/**/*.editor[ca]ss", _styles);
 }
 
-exports.default = series(build, editorStyle, feStyle, watchTask);
-exports.watch = series(editorStyle, feStyle, watchTask);
-exports.build = series(build, editorStyle, feStyle);
-exports.styles = series(editorStyle, feStyle, watchTask);
+exports.default = series(_build, _styles, watchTask);
+exports.build = series(_build, _styles);
